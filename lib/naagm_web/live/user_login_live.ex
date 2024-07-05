8664c68189b010f37cef6c678547ae9ad716ed47
Nicolas Knoebber <knoebber@gmail.com>
2024-07-05 10:16:48 -0700

    Add login

---
+++ lib/naagm_web/live/user_login_live.ex
@@ -0,0 +1,25 @@
+defmodule NaagmWeb.UserLoginLive do
+  use NaagmWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    email = Phoenix.Flash.get(socket.assigns.flash, :email)
+    form = to_form(%{"email" => email}, as: "user")
+    {:ok, assign(socket, form: form), temporary_assigns: [form: form]}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
+        <.input field={@form[:email]} type="email" label="Email" required />
+        <.input field={@form[:password]} type="password" label="Password" required />
+        <:actions>
+          <.button phx-disable-with="Logging in..." class="w-full">
+            Log in <span aria-hidden="true">→</span>
+          </.button>
+        </:actions>
+      </.simple_form>
+    </div>
+    """
+  end
+end
--- lib/naagm_web/router.ex
+++ lib/naagm_web/router.ex
@@ -13,16 +13,6 @@ defmodule NaagmWeb.Router do
     plug :fetch_current_user
   end
 
-  scope "/", NaagmWeb do
-    pipe_through [:browser]
-
-    live "/", HomeLive
-    live "/upload", UploadLive
-    live "/rsvp", RSVPLive
-    live "/photos", PhotosLive
-    live "/about", AboutLive
-  end
-
   ## Authentication routes
 
   scope "/", NaagmWeb do
@@ -30,10 +20,7 @@ defmodule NaagmWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{NaagmWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
 
     post "/users/log_in", UserSessionController, :create
@@ -44,8 +31,11 @@ defmodule NaagmWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{NaagmWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/", HomeLive
+      live "/upload", UploadLive
+      live "/rsvp", RSVPLive
+      live "/photos", PhotosLive
+      live "/about", AboutLive
     end
   end
 
@@ -53,11 +43,5 @@ defmodule NaagmWeb.Router do
     pipe_through [:browser]
 
     delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{NaagmWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
   end
 end
