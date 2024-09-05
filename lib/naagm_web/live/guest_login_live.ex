6a062f3c128d6500f85462560b5da1400ca2eacb
Nicolas Knoebber <knoebber@gmail.com>
2024-09-04 22:16:23 -0700

    Improve style

--- lib/naagm_web/live/guest_login_live.ex
+++ lib/naagm_web/live/guest_login_live.ex
@@ -2,7 +2,7 @@ defmodule NaagmWeb.GuestLoginLive do
   use NaagmWeb, :live_view
 
   def mount(_, _, socket) do
-    {:ok, assign(socket, :body_class, "sunset")}
+    {:ok, assign(socket, :body_class, "sunset-bg")}
   end
 
   def render(assigns) do
@@ -22,7 +22,20 @@ defmodule NaagmWeb.GuestLoginLive do
           <input type="hidden" name="user[email]" value="guest@example.com" />
           <input type="hidden" name="user[from]" value={~p"/login"} />
           <.button phx-disable-with="Logging in..." tabindex="0">
-            👉
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
           </.button>
         </div>
       </.form>
