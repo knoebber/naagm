64215b8c21477e64b92f1ace6e6009c68340c3e6
Nicolas Knoebber <knoebber@gmail.com>
2024-12-22 11:24:23 -0800

    Extract upload into live component

---
+++ lib/naagm_web/components/upload_form.ex
@@ -0,0 +1,130 @@
+defmodule NaagmWeb.UploadForm do
+  alias Naagm.S3
+  require Logger
+  use NaagmWeb, :live_component
+
+  defp error_to_string(reason) do
+    case reason do
+      :external_client_failure -> "Failed to contact S3 server"
+      :not_accepted -> "File type not supported"
+      :too_large -> "Too large"
+      :too_many_files -> "You have selected too many files"
+    end
+  end
+
+  defp presign_upload(entry, socket) do
+    %{url: url, fields: fields} =
+      S3.presign_upload(%{
+        client_name: entry.client_name,
+        prefix: socket.assigns.prefix
+      })
+
+    {:ok, %{uploader: "S3", url: url, fields: fields}, socket}
+  end
+
+  @impl Phoenix.LiveComponent
+  def update(assigns, socket) do
+    socket = assign(socket, assigns)
+    admin? = Naagm.Accounts.admin?(socket.assigns.current_user)
+    prefixes = S3.prefixes()
+
+    prefix =
+      if admin? do
+        hd(prefixes)
+      else
+        S3.guest_gallery_prefix()
+      end
+
+    {
+      :ok,
+      socket
+      |> assign(:admin?, admin?)
+      |> assign(:prefixes, prefixes)
+      |> assign(:prefix, prefix)
+      |> assign(:uploaded_files, [])
+      |> allow_upload(:content,
+        accept: ~w(.jpg .jpeg .webp .png),
+        max_entries: 10,
+        external: &presign_upload/2
+      )
+    }
+  end
+
+  @impl Phoenix.LiveComponent
+  def handle_event("select-prefix", %{"prefix" => prefix}, socket) do
+    {:noreply, assign(socket, :prefix, prefix)}
+  end
+
+  @impl Phoenix.LiveComponent
+  def handle_event("validate", params, socket) do
+    dbg(params)
+    {:noreply, assign(socket, :gallery?, params["gallery"] == "true")}
+  end
+
+  @impl Phoenix.LiveComponent
+  def handle_event("save", _params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :uploads, fn entry, _ ->
+        {:ok, Logger.info("Uploaded #{inspect(entry)}")}
+      end)
+
+    send(self(), {:info_message, "Upload finished"})
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+  end
+
+  @impl Phoenix.LiveComponent
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :content, ref)}
+  end
+
+  @impl Phoenix.LiveComponent
+  def render(assigns) do
+    ~H"""
+    <div phx-drop-target={@uploads.content.ref} class="container">
+      <.simple_form :if={@admin?} phx-change="select-prefix" for={%{}} phx-target={@myself}>
+        <.input label="Folder" name="prefix" type="select" value={@prefix} options={@prefixes} />
+      </.simple_form>
+      <.form
+        class="upload-form"
+        id="upload-form"
+        phx-submit="save"
+        phx-change="validate"
+        for={%{}}
+        phx-target={@myself}
+      >
+        <label for={@uploads.content.ref}>
+          Drop a file or<.live_file_input upload={@uploads.content} />
+        </label>
+        <button :if={length(@uploads.content.entries) > 0} class="button" type="submit">
+          Upload
+        </button>
+      </.form>
+      <section class="upload-entries">
+        <%= for entry <- @uploads.content.entries do %>
+          <article class="upload-entry">
+            <figure>
+              <.live_img_preview entry={entry} />
+              <figcaption>{entry.client_name}</figcaption>
+            </figure>
+            <progress value={entry.progress} max="100">{entry.progress}%</progress>
+            <button
+              type="button"
+              phx-click="cancel-upload"
+              phx-value-ref={entry.ref}
+              aria-label="cancel"
+            >
+              &times;
+            </button>
+            <%= for err <- upload_errors(@uploads.content, entry) do %>
+              <p class="alert alert-danger">{error_to_string(err)}</p>
+            <% end %>
+          </article>
+        <% end %>
+        <%= for err <- upload_errors(@uploads.content) do %>
+          <p class="alert alert-danger">{error_to_string(err)}</p>
+        <% end %>
+      </section>
+    </div>
+    """
+  end
+end
--- lib/naagm_web/live/upload_live.ex
+++ lib/naagm_web/live/upload_live.ex
@@ -1,112 +1,15 @@
 defmodule NaagmWeb.UploadLive do
-  alias Naagm.S3
   use NaagmWeb, :live_view
 
-  defp error_to_string(reason) do
-    case reason do
-      :external_client_failure -> "Failed to contact S3 server"
-      :not_accepted -> "File type not supported"
-      :too_large -> "Too large"
-      :too_many_files -> "You have selected too many files"
-    end
-  end
-
   @impl Phoenix.LiveView
-  def mount(_params, _session, socket) do
-    admin? = Naagm.Accounts.admin?(socket.assigns.current_user)
-    prefixes = S3.prefixes()
-
-    prefix =
-      if admin? do
-        hd(prefixes)
-      else
-        S3.guest_gallery_prefix()
-      end
-
-    {:ok,
-     socket
-     |> assign(:admin?, admin?)
-     |> assign(:prefixes, prefixes)
-     |> assign(:prefix, prefix)
-     |> assign(:uploaded_files, [])
-     |> allow_upload(:content,
-       accept: ~w(.jpg .jpeg .webp .png),
-       max_entries: 10,
-       external: &presign_upload/2
-     )}
-  end
-
-  defp presign_upload(entry, socket) do
-    %{url: url, fields: fields} =
-      S3.presign_upload(%{
-        client_name: entry.client_name,
-        prefix: socket.assigns.prefix
-      })
-
-    {:ok, %{uploader: "S3", url: url, fields: fields}, socket}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_event("select-prefix", %{"prefix" => prefix}, socket) do
-    {:noreply, assign(socket, :prefix, prefix)}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_event("validate", params, socket) do
-    {:noreply, assign(socket, :gallery?, params["gallery"] == "true")}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_event("save", _params, socket) do
-    {:noreply, socket |> put_flash(:info, "Upload finished")}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
-    {:noreply, cancel_upload(socket, :content, ref)}
+  def handle_info({:info_message, message}, socket) do
+    {:noreply, put_flash(socket, :info, message)}
   end
 
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div phx-drop-target={@uploads.content.ref} class="container">
-      <.form :if={@admin?} phx-change="select-prefix" for={%{}}>
-        <.input name="prefix" type="select" value={@prefix} options={@prefixes} />
-      </.form>
-      <.form class="upload-form" id="upload-form" phx-submit="save" phx-change="validate" for={%{}}>
-        <label for={@uploads.content.ref}>
-          Drop a file or<.live_file_input upload={@uploads.content} />
-        </label>
-        <button :if={length(@uploads.content.entries) > 0} class="button" type="submit">
-          Upload
-        </button>
-      </.form>
-      <section class="upload-entries">
-        <%= for entry <- @uploads.content.entries do %>
-          <article class="upload-entry">
-            <figure>
-              <.live_img_preview entry={entry} />
-              <figcaption>{entry.client_name}</figcaption>
-            </figure>
-            <progress value={entry.progress} max="100">{entry.progress}%</progress>
-            <button
-              type="button"
-              phx-click="cancel-upload"
-              phx-value-ref={entry.ref}
-              aria-label="cancel"
-            >
-              &times;
-            </button>
-            <%= for err <- upload_errors(@uploads.content, entry) do %>
-              <p class="alert alert-danger">{error_to_string(err)}</p>
-            <% end %>
-          </article>
-        <% end %>
-        <%= for err <- upload_errors(@uploads.content) do %>
-          <p class="alert alert-danger">{error_to_string(err)}</p>
-        <% end %>
-      </section>
-    </div>
+    <.live_component module={NaagmWeb.UploadForm} id="upload-form" current_user={@current_user} />
     """
   end
 end
